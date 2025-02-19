21d1b6bb406ff7c5eafd6dcfcf196a537d059711
losthope <arsen.melnuchyk@gmail.com>
2025-02-19 12:05:57 +0200

    feat: create albums

--- src/app/forms.py
+++ src/app/forms.py
@@ -96,5 +96,26 @@ class UploadForm(FlaskForm):
                 message="File extension isn't in the list: jpg, jpeg, png",
             ),
         ],
+        render_kw={"multiple": True}
     )
     submit = SubmitField("Upload")
+
+
+class AlbumForm(FlaskForm):
+    name = StringField(
+        "Name",
+        validators=[
+            DataRequired(),
+            Length(min=6, max=50, message="The name of the album is too long"),
+        ],
+        render_kw={"placeholder": "Enter the name for an album"},
+    )
+    category = StringField(
+        "Category",
+        validators=[
+            DataRequired(),
+            Length(min=4, max=20, message="The category for the album is too long"),
+        ],
+        render_kw={"placeholder": "Enter the category for an album"},
+    )
+    submit = SubmitField("Create album")
--- src/app/views.py
+++ src/app/views.py
@@ -1,11 +1,11 @@
 from flask import render_template, redirect, url_for, flash, abort, request
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from flask_bcrypt import generate_password_hash, check_password_hash
 from sqlalchemy.exc import IntegrityError
 from b2sdk.v2 import B2Api, B2Folder
 from app import app, b2, db
-from app.forms import RegisterForm, LoginForm, UploadForm
-from app.models import User, Photo
+from app.forms import RegisterForm, LoginForm, UploadForm, AlbumForm
+from app.models import User, Photo, PhotoAlbum
 
 
 @app.get("/")
@@ -14,19 +14,69 @@ def home():
     return render_template("home.html", title=title)
 
 
-@app.route("/photos/")
+@app.get("/photos/")
 @login_required
 def photos():
     title: str = "Photos"
+
+    form = AlbumForm()
+
+    albums = db.session.query(PhotoAlbum).all()
+
+    return render_template("photos.html", title=title, form=form, albums=albums)
+
+
+@app.get("/photos/<album_id>/")
+@login_required
+def album_page(album_id):
+    title: str = "Upload photo"
     form = UploadForm()
+
+    return render_template("album.html", title=title, form=form)
+
+
+@app.get("/photos/create")
+@login_required
+def create_album():
+    title: str = "Create album"
+    form = AlbumForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for("process_album"))
+
+    return render_template("create.html", title=title, form=form)
+
+
+@app.post("/photos/create")
+@login_required
+def process_album():
+    try:
+        form = AlbumForm(request.form)
+        name = form.name.data
+        category = form.category.data
+        
+        album = PhotoAlbum(name=name, user_id=current_user.get_id())
+        db.session.add(album)
+        db.session.commit()
+
+    except IntegrityError:
+        flash("Album with a given name already exist", "error")
+        return redirect(url_for("photos"))
+
+    flash("Album was created successfully", "success")
+    return redirect(url_for("photos"))
+
+
+@app.post("/photos/<int:album_id>/")
+@login_required
+def process_upload(album_id: int):
+    files = request.files.getlist("file")
+
+    for f in files:
+        print(f.filename)
 
     bucket = b2.get_bucket_by_id(app.config["BUCKET_ID"])
 
-    return render_template("photos.html", title=title, form=form)
-
-
-@app.post("/photos/")
-def upload_photo():
     return redirect(url_for("photos"))
 
 
@@ -84,7 +134,7 @@ def process_login():
     )
 
     if not check_password_hash(user.password, form.password.data):
-        flash("Password is wrong or user doesn't exist", "error")
+        flash("The password is wrong or user doesn't exist", "error")
         return redirect(url_for("login_page"))
 
     login_user(user, remember=form.stay_logged_in.data)
