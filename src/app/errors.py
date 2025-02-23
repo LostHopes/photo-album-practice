7521709e7072a55052000533555e3b73f1b77bf5
losthope <arsen.melnuchyk@gmail.com>
2025-02-23 15:51:12 +0200

    refactor: create title for error pages

--- src/app/errors.py
+++ src/app/errors.py
@@ -4,9 +4,11 @@ from app import app
 
 @app.errorhandler(404)
 def page_not_found(msg):
-    return render_template("error.html", msg=msg)
+    title: str = "Error 404"
+    return render_template("error.html", msg=msg, title=title)
 
 
 @app.errorhandler(401)
 def user_unauthorized(msg):
-    return render_template("error.html", msg=msg)
+    title: str = "Error 401"
+    return render_template("error.html", msg=msg, title=title)
