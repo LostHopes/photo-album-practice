8ffc3ec59bc7f5e7625f12f737f751da011d8fd6
losthope <arsen.melnuchyk@gmail.com>
2025-03-11 13:47:10 +0200

    refactor: changed config to ProdConfig for the production branch

--- src/wsgi.py
+++ src/wsgi.py
@@ -2,7 +2,7 @@ from app import create_app
 from app import config
 
 
-app = create_app(config.DevConfig)
+app = create_app(config.ProdConfig)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
