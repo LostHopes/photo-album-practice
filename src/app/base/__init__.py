f06bd72536d1bb3573ebdf83dfcdacf0cf46411b
losthope <arsen.melnuchyk@gmail.com>
2025-04-23 18:28:36 +0300

    style: format code

--- src/app/base/__init__.py
+++ src/app/base/__init__.py
@@ -2,10 +2,7 @@ from flask import Blueprint
 
 
 base_bp = Blueprint(
-    "base_bp",
-    __name__,
-    template_folder="templates/base",
-    static_folder="static/base"
+    "base_bp", __name__, template_folder="templates/base", static_folder="static/base"
 )
 
 from app.base import views, errors
